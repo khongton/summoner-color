49c882089083c86731b4b1a7af8b2e61a6dc10f0
khongton <khongtongsak@hotmail.com>
2016-12-02 23:46:09 -0800

    Started implementation of Match List functions
    Added code to call matchlist and matchdetails API.
    Currently working on trying to debug POST and JSON methods

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ app = Flask(__name__)
 def hello():
 	return render_template('index.html')
 
-@app.route("/signUp")
-def signUp():
-	return render_template('signUp.html')
-
 @app.route("/summoner")
 def summonerLookup():
 	return render_template('lookup.html')
@@ -24,11 +20,21 @@ def lookup():
 	response = requests.get(url)
 	return json.dumps(response.json())
 
-@app.route('/signUpUser', methods=['POST'])
-def signUpUser():
-	user = request.form['username']
-	password = request.form['password']
-	return json.dumps({'status':'OK','user':user,'password':password})
+@app.route("/matchlist", methods=['POST'])
+def matchLookup():
+	match = request.json['summonerid']
+	filterMatchIndices = '?beginIndex=0&endIndex=5'
+	url = config.baseurl + config.apis['matchlist'] + str(match) + filterMatchIndices + config.apikey
+	print(url)
+	response = requests.get(url)
+	return json.dumps(response.json())
+
+@app.route("/matchdetail", methods=['POST'])
+def matchDetails():
+	matchID = request.json['matchid']
+	url = config.baseurl + config.apis['matchdetail'] + str(matchID) + config.apikey
+	response = requests.get(url)
+	return json.dumps(respons.json())
 
 if __name__ == '__main__':
 	app.run()
--- static/js/lookup.js
+++ static/js/lookup.js
@@ -4,6 +4,8 @@ $(function() {
 	})
 });
 
+
+//I'm making a lot of AJAX calls - maybe template them or something
 function searchSummoner() {
 	var summonerName = $('#summoner-name').val();
 	$.ajax({
@@ -13,11 +15,55 @@ function searchSummoner() {
 		success: function(response) {
 			var jsonObj = $.parseJSON(response);
 			var profileID = jsonObj[summonerName.toLowerCase()].id;
-			$('#response').html('<p>Your summoner ID is: ' + profileID + '</p>');
+			$('#response').html('<p>Summoner Information: ' + profileID + '</p>');
+			searchMatches(profileID);
 		},
 		error: function(error) {
-			$('#response').html('<p>Looks like something went wrong!</p>');
-			console.log(error);
+			errorOutput(error);
 		}
 	})
+}
+
+function searchMatches(summonerID) {
+	console.log(summonerID);
+	var jsonID = {'summonerid' : summonerID};
+	$.ajax({
+		url: '/matchlist',
+		data: JSON.stringify(jsonID),
+		contentType: 'application/json;charset=UTF-8',
+		type: 'POST',
+		success: function(response) {
+			//JSON object contains list of ranked games user has played
+			var matchJSON = $.parseJSON(response);
+			console.log(matchJSON);
+		//	for (index in matchJSON.matches) {
+		//		findMatchDetail(matches[index].matchID);
+			//}
+		},
+		error: function(error) {
+			errorOutput(error);
+		}
+	});
+}
+
+function findMatchDetail(matchID) {
+	console.log('Match ID: ' + matchID);
+	var jsonMatch = {'matchid' : matchID};
+	$.ajax({
+		url: '/matchdetail',
+		data: JSON.stringify(jsonMatch),
+		contentType: 'application/json;charset=UTF-8',
+		type: 'POST',
+		success: function(response) {
+			console.log(response);
+		},
+		error: function(error) {
+			errorOutput(error);
+		}
+	})
+}
+
+function errorOutput(error) {
+	$('#response').html('<p>Looks like something went wrong!</p>');
+	console.log(error);
 }
