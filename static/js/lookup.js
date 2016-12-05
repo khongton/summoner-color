edf30afb5ed403e9a0294dc2ba6dee7e3497fd1a
khongton <khongtongsak@hotmail.com>
2016-12-04 21:35:40 -0800

    Quick implementation of a simple D3 line graph

--- static/js/lookup.js
+++ static/js/lookup.js
@@ -1,12 +1,54 @@
 var summonerName = '';
 var participantID = null;
 var playerFrames = []
+
 $(function() {
 	$('button').click(function() {
+		playerFrames = [];
 		searchSummoner();
-	})
+	});
 });
 
+$(document).ajaxStop(function() {
+	var data = formatData();
+	var graph = d3.select('#graph'),
+		WIDTH = 1000,
+		HEIGHT = 500,
+		MARGINS = {
+			top: 20,
+			right: 20,
+			bottom: 20,
+			left: 50
+		},
+		xRange = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(data, function(dataPoint) {
+			return dataPoint.x;
+		}), d3.max(data, function(dataPoint) {
+			return dataPoint.x;
+		})]),
+		yRange = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(data, function(dataPoint) {
+			return dataPoint.y;
+		}), d3.max(data, function(dataPoint) {
+			return dataPoint.y;
+		})]),
+		xAxis = d3.axisBottom().scale(xRange).tickSize(1),
+		yAxis = d3.axisLeft().scale(yRange);
+	var lineFunction = d3.line().x(function(data) { return xRange(data.x);}).y(function(data) { return yRange(data.y);})
+
+	//These two lines attach the graph to our HTML document
+	graph.append('svg:g').attr('class', 'x-axis').attr('transform', 'translate(0,' + (HEIGHT - MARGINS.bottom) + ')').call(xAxis);
+	graph.append('svg:g').attr('class', 'y-axis').attr('transform', 'translate(' + (MARGINS.left) + ',0)').call(yAxis);
+	graph.append('svg:path').attr('d', lineFunction(data)).attr('stroke', 'blue').attr('stroke-width', 2).attr('fill', 'none');
+});
+
+function formatData() {
+	var data = []
+	for (var idx = 0; idx < playerFrames.length; idx++) {
+		var curGold = playerFrames[idx].currentGold;
+		var dataPoint = {x: idx, y: curGold};
+		data.push(dataPoint);
+	}
+	return data;
+}
 
 function searchSummoner() {
 	summonerName = $('#summoner-name').val();
