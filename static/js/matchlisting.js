5344f16451fad1b7ff84d1810f3ee903ad3ca425
khongton <khongtongsak@hotmail.com>
2016-12-09 02:23:37 -0800

    Removed unnecessary JavaScript from templates

--- static/js/matchlisting.js
+++
@@ -1,9 +0,0 @@
-function listMatches(matchHistory) {
-	var list = $('#listing');
-	$.each(matchHistory.games, function(index, value) {
-		var date = new Date(value.createDate);
-		var dateString = date.toLocaleDateString();
-		var timeString = date.toLocaleTimeString();
-		list.append('<div id="match-' + index + '">' + dateString + ' ' + timeString + '</div>');
-	});
-}
