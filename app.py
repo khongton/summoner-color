b22eefe91f1ed7b3b484f91f947f2530f13adfd6
khongton <khongtongsak@hotmail.com>
2016-12-09 20:20:09 -0800

    Added templating for search results page

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def getMatchHistory(summonerId):
 	return jsonObj
 
 @app.template_filter('localDate')
-def localDate(seconds): #seconds = milliseconds from epoch
+def localDate(seconds): #seconds = milliseconds from epoch as defined by riot api
 	return datetime.fromtimestamp(seconds / 1e3).strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
